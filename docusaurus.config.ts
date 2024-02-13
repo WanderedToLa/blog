1d02234a155b3d0a9dce4d7950502362781d3e70
WanderedToLa <rladntjr9808@gmail.com>
2024-02-13 12:14:25 +0900

    fix edit url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -29,15 +29,11 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/WanderedToLa/logs/tree/master/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/WanderedToLa/logs/tree/master/',
         },
         theme: {
           customCss: './src/css/custom.css',
